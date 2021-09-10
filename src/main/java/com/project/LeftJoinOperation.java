f10abb1e72f46472d43e613476a3587ab0d77480
Daniilart01 <89593109+Daniilart01@users.noreply.github.com>
2021-09-10 10:00:51 +0300

    Making Left- and Right JoinOperation classes correct with Generics and making them non-abstract

--- src/main/java/com/project/LeftJoinOperation.java
+++ src/main/java/com/project/LeftJoinOperation.java
@@ -1,4 +1,10 @@
 package com.project;
 
-public abstract class LeftJoinOperation<K, V1, V2> implements JoinOperation {
+import java.util.Collection;
+
+public class LeftJoinOperation<K, V1, V2> implements JoinOperation<DataRow<K,V1>, DataRow<K,V2>, JoinedDataRow<K,V1,V2>>{
+    @Override
+    public Collection<JoinedDataRow<K, V1, V2>> join(Collection<DataRow<K, V1>> leftCollection, Collection<DataRow<K, V2>> rightCollection) {
+        return null;
+    }
 }
--- src/main/java/com/project/RightJoinOperation.java
+++ src/main/java/com/project/RightJoinOperation.java
@@ -1,4 +1,11 @@
 package com.project;
 
-public abstract class RightJoinOperation<K, V1, V2> implements JoinOperation {
+import java.util.Collection;
+
+public class RightJoinOperation<K, V1, V2> implements JoinOperation<DataRow<K,V1>, DataRow<K,V2>, JoinedDataRow<K,V1,V2>> {
+
+    @Override
+    public Collection<JoinedDataRow<K, V1, V2>> join(Collection<DataRow<K, V1>> leftCollection, Collection<DataRow<K, V2>> rightCollection) {
+        return null;
+    }
 }
